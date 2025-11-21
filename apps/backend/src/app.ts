cede3f475bae8fe86017c1b8d0f9d92514c2add1
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2025-11-21 10:45:06 +0000

    Check cors

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import cors from "cors";
 import routes from "./routes/index";
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api", routes);
 
 export default app;
