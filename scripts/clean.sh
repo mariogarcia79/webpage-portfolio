82362309d0b63536fc542a47ce99263ad2dfe27e
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2025-12-23 00:17:14 +0000

    Adapt to docker swarm

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-docker system prune -a --volumes --exclude mongo-data
+docker system prune -a --volumes
 docker volume rm prometheus-data
 #docker network prune -f
 #docker builder prune -f
