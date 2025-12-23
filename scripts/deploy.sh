1c4d7402823e7860b338064788bff5c1cc3ad7b0
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2025-12-23 00:14:11 +0000

    Adapt to docker swarm

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,12 @@
 git pull
 ./scripts/setup-prometheus.sh
 
-# Rebuild and redeploy containers
+# Build images
 docker compose build --no-cache
-docker compose down
-# Clean up old containers and volumes
-./scripts/clean.sh
-docker compose up -d 
+
+# Remove old stack (if exists)
+docker stack rm webpage
+sleep 5
+
+# Deploy the stack
+docker stack deploy -c docker-compose.yml webpage
