5a858b93ed58f45a38ca90d44e5e7704bbd3aaa0
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2025-12-23 13:29:13 +0000

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,7 @@ git pull
 # Rebuild and redeploy containers
 docker compose build --no-cache
 docker compose down
+docker compose up -d 
+
 # Clean up old containers and volumes
 ./scripts/clean.sh
-docker compose up -d 
