1afe0028afc94b8de30b910be28495ca6ff3e4df
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2025-12-25 21:43:41 +0000

    fix

--- scripts/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-docker system prune -a --volumes
-docker volume rm prometheus-data
-#docker network prune -f
-#docker builder prune -f
-echo "Cleaned up old containers, volumes, networks, and build cache."
