01fca3fb6b39d55d2c2c309754b43d4d233e8b29
mariogarcia79 <74426959+mariogarcia79@users.noreply.github.com>
2026-01-02 15:28:57 +0000

    fix

--- frontend/src/components/error/NotFound.tsx
+++ frontend/src/components/error/NotFound.tsx
@@ -1,10 +1,19 @@
 function NotFound() {
   return (
-    <div>
+    <div style={containerStyles}>
       <h1>404 - Page Not Found</h1>
-      <p>Go enumerate my ass.</p>
+      <p>Go enumerate my ass ;)</p>
     </div>
   );
 }
+
+const containerStyles: React.CSSProperties = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '90vh',
+  textAlign: 'center',
+};
 
 export default NotFound;
